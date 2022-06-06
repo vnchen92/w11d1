9cc425964aa4e6deadc8ef610e1faa51af3fa934
nikumar1206 <nikumar1206@gmail.com>
2022-06-06 10:02:47 -0400

    created repo

---
+++ src/entry.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  const hello = <h1>Hello, Welcome to React!</h1>;
+
+  ReactDOM.render(hello, root);
+});
